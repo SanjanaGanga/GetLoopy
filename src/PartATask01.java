0f114c0adf756fc0410e27ef1c7b066c2d139d55
Sanjana Ganga <gangasc@mail.uc.edu>
2026-06-25 22:05:58 -0400

    Task 1 Part A Complete

--- src/PartATask01.java
+++ src/PartATask01.java
@@ -1,13 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class PartATask01 {
+    public static void main(String[] args) {
+        // for loop that counts up by 1 from 0 to 30
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
