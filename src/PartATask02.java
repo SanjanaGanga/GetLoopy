fbea2c4ed3a2eeb707a8eefcea915d7f5adaeea7
Sanjana Ganga <gangasc@mail.uc.edu>
2026-06-25 22:11:15 -0400

    Part A Task 2 Complete

---
+++ src/PartATask02.java
@@ -0,0 +1,8 @@
+public class PartATask02 {
+    public static void main(String[] args) {
+        // for loop that counts down by 1 from 0 to 30
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
